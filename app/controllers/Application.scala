1034961825f2dcf2f1222aaf095c858523eae176
unknown <fer.savi1326@gmail.com>
2015-10-12 16:29:47 -0500

    haciendo primer commit de carperta  App del proyecto

---
+++ app/controllers/Application.scala
@@ -0,0 +1,32 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.data.Form
+import play.api.data.Forms._
+import models.{Person,DB}
+import play.api.libs.json.Json
+
+class Application extends Controller {
+
+  def index = Action {
+    Ok(views.html.index("Your new application is ready."))
+  }
+  
+  val formPerson: Form[Person] = Form {
+    mapping(
+        "name" -> text,
+        "age" -> number
+    )(Person.apply)(Person.unapply)
+  }
+    def addPerson = Action{ implicit request =>
+        val person = formPerson.bindFromRequest.get
+        DB.save(person)
+        Redirect(routes.Application.index)
+    }
+    
+    def getPersons = Action{
+        val persons = DB.query[Person].fetch
+        Ok(Json.toJson(persons))
+    }
+}
